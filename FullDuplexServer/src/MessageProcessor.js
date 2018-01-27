9c8345be34bad2c6419ea822fbfbd72163ee7b18
Spoffy <spoffeh@gmail.com>
2018-01-27 10:12:35 +0000

    Initial Commit 2 - Changed to PHPStorm

--- FullDuplexServer/src/MessageProcessor.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class MessageProcessor {
-    processMessage(message) {
-        console.log(message.author.username, ":", message.content);
-    }
-}
-exports.MessageProcessor = MessageProcessor;
-//# sourceMappingURL=MessageProcessor.js.map
