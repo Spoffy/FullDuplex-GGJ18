ace4994da7b64843f8fa2aa4a8f79f5ebef1a238
Spoffy <Spoffeh@gmail.com>
2018-01-27 21:34:08 +0000

    Improved winning message

--- FullDuplexServer/src/GameManager.ts
+++ FullDuplexServer/src/GameManager.ts
@@ -84,7 +84,9 @@ export class GameManager {
     }
 
     win(game: Game) {
-        game.avatarPlayer.channel.send("You did it!");
-        game.remotePlayer.channel.send("You did it!");
+        game.avatarPlayer.channel.send("Congratulations, you reached the exit and escape. Stay tuned for more levels," +
+            " or try the game from the other perspective!", {reply: game.avatarPlayer.user});
+        game.remotePlayer.channel.send("Congratulations, you reached the exit and escape. Stay tuned for more levels," +
+            " or try the game from the other perspective!", {reply: game.remotePlayer.user});
     }
 }
