adf8a7526201bec446b049917cdffe89282e8b8b
Spoffy <spoffeh@gmail.com>
2018-01-27 10:58:20 +0000

    MessageProcessor ignores messages from itself.

--- FullDuplexServer/src/MessageProcessor.ts
+++ FullDuplexServer/src/MessageProcessor.ts
@@ -2,6 +2,11 @@
 
 export class MessageProcessor {
     public processMessage(message: Discord.Message) {
+        if(message.author.id == message.client.user.id) {
+            console.log("Not replying: This bot doesn't talk to itself.");
+            return;
+        }
         console.log(message.author.username, ":", message.content);
+        message.channel.send("Bananas!").catch(() => {});
     }
 }
